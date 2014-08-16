69514885c8bfa39e69f9b6343811b287d153e1a8
wiscdrew <adanthony@gmail.com>
2014-08-16 13:16:40 -0500

    Programming Assignment 2
    Caching a matrix inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Provides functions for calculating and storing an inverse of a matrix for 
+## allowing for quicker computation.
 
-## Write a short comment describing this function
+## provides the functions needed for setting  a matrix, getting the matrix,
+## setting the inverse of the matrix, and getting the  inverse of the matrix.
+## imat is Inverse of MATrix
 
 makeCacheMatrix <- function(x = matrix()) {
+        imat <- NULL
+        
+        ## function for setting matrix
+        set <- function(y){
+                x <<- y
+                imat <<- NULL
+        }
+        
+        ## function for getting matrix
+        get <- function(){
+                x
+        }
+        
+        ## function for setting inverse of matrix
+        setinverse <- function(inverse){
+                imat <<- inverse
+        }
+        
+        ## function for getting inverse of matrix
+        getinverse <- function(){
+                imat
+        }
 
 }
 
 
-## Write a short comment describing this function
+## This function checks if the inverse of a matrix has been cached and
+## returns cached value. If the inverse has not been set, this function
+## calculates the inverse of the passed matrix, sets the inverse matrix value
+## in the cache, and returns the inverse of the matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        ## gets cached value of inverse
+        imat <- x$getinverse()
+        
+        ## checks if inverse has already been set. If yes, returns cached value
+        if(!is.null(imat)){
+                message("Getting cached data")
+                return(imat)
+        }
+        
+        ## If inverse has not been set, sets inverse...
+        
+        data <- x$get()
+        imat <- solve(data,...)
+        x$setinverse(imat)
+        
+        ## And returns the inverse
+        imat
 }
